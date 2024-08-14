9a8f52422afc32a491f133872c868fb6e91441e0
developer-mustafa <mustafarahman792@gmail.com>
2024-08-14 09:51:29 +0600

    change info job

---
+++ app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+//printf("hello 2nd year");
+int chaler_dam = 80;
+
+printf("ekhon chaler dam holo = %d",chaler_dam);
+
+    return 0;
+}
--- index.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-   printf("Welcome");
-    return 0;
-}
